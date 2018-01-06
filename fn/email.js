e26a9d8f4a9e19531d6fcbe332db20b7282f390a
TDLAM <tdlam123@gmail.com>
2018-01-06 21:36:44 +0700

    first implement send email transaction cofirm

--- fn/email.js
+++ fn/email.js
@@ -43,6 +43,49 @@ exports.sendEmail = function (emailaddress, address, host){
 	});
 }
 
+//send email transaction confirm with content:
+const content= {
+    transactionID: 1,
+    sender: "tdlam123@gmail.com",
+    sender_address: 'aaa',
+    receiver_address: 'bbb',
+    recerver: 'ttlam'
+}
+const renderHTML_TransactionConfirm = function (content ) {
+    var html = '' +
+        'Hello,' +
+        '<br>Your Transaction information below:' +
+        'TransactionID:'+'<strong>"+content.transactionID+"<strong> ' +
+        'Your wallet address:'+'<strong>"+content.sender_address+"<strong> ' +
+        'Receive address:'+'<strong>"+content.receiver_address+"<strong> ' +
+        '<strong>"+address+"<strong> ' +
+        'Please Click on the link to verify your email.<br>' +
+        '<a href="+link+">Click here to confirm this transaction</a>'
+    return html;
+}
+
+exports.sendEmailTransactionConfirm = function (content){
+    List_Waiting.push({
+        waiting_email: emailaddress,
+        verify_number: address
+    });
+    console.log('List_Waiting: ',List_Waiting);
+    link = "http://"+HOST+"/email/verify?address="+address;
+    mailOptions = {
+        to : emailaddress,
+        subject : "Confirm your Transaction",
+        html : "Hello,<br>Your address is <strong>"+address+"<strong> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+    }
+    transport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        } else {
+            console.log("Message sent: " + response.message);
+            return response;
+        }
+    });
+}
+
 exports.verify = function(verify_num){
     var d = q.defer();
     List_Waiting.forEach(function(element){
