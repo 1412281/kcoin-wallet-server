a7a8087cd32d00787509bbcc5e1df369b6438d25
TDLAM <tdlam123@gmail.com>
2018-01-07 11:39:31 +0700

    email confirm have not update status from PENDING to others

--- fn/email.js
+++ fn/email.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
 var nodemailer = require("nodemailer");
 var smtpTransport = require('nodemailer-smtp-transport');
-var os = require("os");
+var transactionRepo = require('../models/transactionRepo');
 var q = require('q');
+var ultis = require('./utils');
 const HOST = 'localhost:4000';
 const USER = 'tranthienhoang14';
 const PASSWORD = 'malmalmalmal';
@@ -18,15 +17,15 @@ var transport = nodemailer.createTransport(smtpTransport({
         pass: PASSWORD
     }
 }));	
-var rand, mailOptions, link;
-var List_Waiting = [];
+var mailOptions, link;
+var List_Email_Register_Waiting = [];
 
 exports.sendEmail = function (emailaddress, address, host){
-    List_Waiting.push({
+    List_Email_Register_Waiting.push({
         waiting_email: emailaddress,
         verify_number: address
     });
-    console.log('List_Waiting: ',List_Waiting);
+    console.log('List_Waiting: ',List_Email_Register_Waiting);
     link = "http://"+HOST+"/email/verify?address="+address;
     mailOptions = {
         to : emailaddress,
@@ -42,45 +41,38 @@ exports.sendEmail = function (emailaddress, address, host){
 	    }
 	});
 }
-
+//     coin: '4',
+//     status: 'pending',
+//     email_send: 'lamtran2601@gmail.com',
+//     address_receive: '520159ee31cad850eeb6b26c91ab96fd2a9a4333366e38dbcbfa930224e9183a',
+//     date: '2018-01-06T17:37:58.805Z' }
 //send email transaction confirm with content:
-const content= {
-    transactionID: 1,
-    sender: "tdlam123@gmail.com",
-    sender_address: 'aaa',
-    receiver_address: 'bbb',
-    recerver: 'ttlam'
-}
-const renderHTML_TransactionConfirm = function (content ) {
+const renderHTML_TransactionConfirm = function (content, link ) {
     var html = '' +
         'Hello,' +
-        '<br>Your Transaction information below:' +
-        'TransactionID:'+'<strong>"+content.transactionID+"<strong> ' +
-        'Your wallet address:'+'<strong>"+content.sender_address+"<strong> ' +
-        'Receive address:'+'<strong>"+content.receiver_address+"<strong> ' +
-        '<strong>"+address+"<strong> ' +
-        'Please Click on the link to verify your email.<br>' +
-        '<a href="+link+">Click here to confirm this transaction</a>'
+        '<br>Your Transaction information below:</br>' +
+        '<br>Receive address:'+'<strong>'+content.address_receive+'</strong></br>' +
+        '<br>KCoin:'+'<strong>'+content.coin+'</strong></br>' +
+        '<br>Date:'+'<strong>'+Date.parse(content.date).toString()+'</strong></br>' +
+        '<br>Please Click on the link to confirm transaction.</br>' +
+        '<br><a href="'+link+'">'+link+'</a></br>'
     return html;
 }
 
-exports.sendEmailTransactionConfirm = function (content){
-    List_Waiting.push({
-        waiting_email: emailaddress,
-        verify_number: address
-    });
-    console.log('List_Waiting: ',List_Waiting);
-    link = "http://"+HOST+"/email/verify?address="+address;
+exports.sendEmailTransactionConfirm = function (transaction){
+    // hash transaction content
+    var hash = ultis.hash(JSON.stringify(transaction)).toString('hex')
+    link = "http://"+HOST+"/email/transactionconfirm?hash="+hash;
     mailOptions = {
-        to : emailaddress,
+        to : transaction.email_send,
         subject : "Confirm your Transaction",
-        html : "Hello,<br>Your address is <strong>"+address+"<strong> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+        html : renderHTML_TransactionConfirm(transaction, link)
     }
     transport.sendMail(mailOptions, function(error, response){
         if(error){
             console.log(error);
         } else {
-            console.log("Message sent: " + response.message);
+            console.log("transaction email sent: " + response);
             return response;
         }
     });
@@ -88,10 +80,10 @@ exports.sendEmailTransactionConfirm = function (content){
 
 exports.verify = function(verify_num){
     var d = q.defer();
-    List_Waiting.forEach(function(element){
+    List_Email_Register_Waiting.forEach(function(element){
         if (element.verify_number === verify_num) {
             //remove element after verify
-            List_Waiting.splice(List_Waiting.indexOf(element), 1);
+            List_Waiting.splice(List_Email_Register_Waiting.indexOf(element), 1);
             //return email if it has verify_number
             d.resolve(element.waiting_email);
         }
@@ -102,6 +94,24 @@ exports.verify = function(verify_num){
     return d.promise;
 }
 
-exports.getRandom = function (){
-    return rand;
+exports.transactionConfirm = function (hash) {
+    var d = q.defer();
+    transactionRepo.getPendingTrans().then( function (listpending) {
+        if (listpending.length == 0) {
+            d.resolve('confirm fail')
+        }
+        // hash each transaction element and compare with hash input
+        // if equal, update this transaction element
+        listpending.map(function (element) {
+            console.log('pending')
+            var hashfromDB = ultis.hash(JSON.stringify(element)).toString('hex')
+            if (hashfromDB === hash){
+                // update status of element
+                // transactionRepo.
+                d.resolve(element)
+            }
+        })
+    })
+
+    return d.promise;
 }
--- models/transactionRepo.js
+++ models/transactionRepo.js
@@ -2,7 +2,7 @@ var q = require('q'),
     sync = require('../fn/syncBlockchain'),
     db = require('../fn/db_firebase');
 
-const COLLECTION = 'transaction';
+const COLLECTION = 'transactions';
 
 exports.getAllTrans = function(address) {
     d = q.defer();
@@ -25,7 +25,6 @@ exports.getAllTrans = function(address) {
                     });
                 }
             }
-
         });
     });
     // check in all inputs,
@@ -43,6 +42,19 @@ exports.getAllTrans = function(address) {
 
     d.resolve(listOutput);
 
+    return d.promise;
+};
+
+exports.getPendingTrans = function () {
+    d = q.defer();
+    const query = {
+        where: {
+            status: 'pending'
+        }
+    };
+    db.loadFull(COLLECTION, query).then(function (response) {
+        d.resolve(response.data);
+    });
     return d.promise;
 };
 
